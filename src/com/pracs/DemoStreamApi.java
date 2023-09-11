6760f0121a3ea81e92d86645925856e3d54bdd51
Essakimuthu yadav <yadavessakimuthu16@gmail.com>
2023-09-12 00:17:31 +0530

    1 created a Employee model class 2 Used that model in main method and applied stream api concept

--- src/com/pracs/DemoStreamApi.java
+++ src/com/pracs/DemoStreamApi.java
@@ -1,16 +1,52 @@
 package com.pracs;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DemoStreamApi {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(11, 22, 33, 64, 15);
-        long count = list.stream().filter(x -> x % 2 != 0).count();
-        System.out.println("There are total "+count+" number which are even");
+       List<Employee> listOfEmployee = new ArrayList<>();
+       listOfEmployee.add(new Employee("Essakimuthu",24,"Vasai"));
+       listOfEmployee.add(new Employee("Rahul",23,"Andheri"));
+       listOfEmployee.add(new Employee("Roshan",18,"Ghatkopar"));
+       listOfEmployee.add(new Employee("Gautham",20,"Vasai"));
 
-        long count1 = list.stream().count();
-        System.out.println("The size of the list : "+count1);
+
+       //to collect the targeted name from list of employee;
+
+        Scanner scanner = new Scanner(System.in);
+        String employeeName = scanner.next();
+
+        List<Employee> matchedEmpListWithName = listOfEmployee.stream().filter(employee -> employee.getName().equalsIgnoreCase(employeeName)).collect(Collectors.toList());
+        System.out.println(matchedEmpListWithName);
+
+        //filter out the age greater than 23
+
+        List<Employee> empWithAgeGreaterThan23 = listOfEmployee.stream().filter(employee -> employee.getAge() > 23).collect(Collectors.toList());
+        System.out.println(empWithAgeGreaterThan23);
+
+        //make the employee name in Upper Case
+
+        List<String> employeeNameInUpperCase = listOfEmployee.stream().map(employee -> employee.getName().toUpperCase(Locale.ENGLISH)).collect(Collectors.toList());
+        System.out.println(employeeNameInUpperCase);
+
+        //to get the count of employee with same city
+
+        Map<String, Long> listOfEmployeeWithSameCity = listOfEmployee.stream().collect(Collectors.groupingBy(Employee::getCity, Collectors.counting()));
+        System.out.println(listOfEmployeeWithSameCity);
+
+        //get the list of employee with same city;
+
+      List<Employee> listOfEmpWithVasai = listOfEmployee.stream().filter(employee -> employee.getCity().equalsIgnoreCase("vasai")).collect(Collectors.toList());
+        System.out.println(listOfEmpWithVasai);
+
+        //count of emp
+
+        long count = listOfEmployee.stream().count();
+        System.out.println("Total Count of Emp is : "+count);
+
+        List<Employee> evenAgeEmp = listOfEmployee.stream().filter(employee -> employee.getAge() % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenAgeEmp);
 
     }
 }
