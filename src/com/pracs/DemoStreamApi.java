b8cd4f28773359e6ce7d5b89d1fca8fc1bac3cbe
Essakimuthu yadav <yadavessakimuthu16@gmail.com>
2023-09-11 23:46:55 +0530

    Added count functionality from stream api to count the size of list

--- src/com/pracs/DemoStreamApi.java
+++ src/com/pracs/DemoStreamApi.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public class DemoStreamApi {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list = Arrays.asList(11, 22, 33, 64, 15);
         long count = list.stream().filter(x -> x % 2 != 0).count();
         System.out.println("There are total "+count+" number which are even");
+
+        long count1 = list.stream().count();
+        System.out.println("The size of the list : "+count1);
+
     }
 }
